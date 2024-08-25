2f4a4921f1e5e30f2763e128efb0c77beb749ee6
stevesdiary <stephenoyeyemi@gmail.com>
2024-08-25 16:58:20 +0100

    Create Program.cs

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿//Console application is the type of application that does not have graphical interface
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+namespace HelloWorld
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello world! of C#");
+		}
+	}
+} 

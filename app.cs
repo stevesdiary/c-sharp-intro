3f6f62b445866e31dad9498803f7ad040cafe2b6
stevesdiary <stephenoyeyemi@gmail.com>
2024-08-27 02:23:30 +0100

    Overflowing

--- app.cs
+++ app.cs
@@ -1,12 +1,5 @@
-//c# is a case sensitive language
-// to declare  variables or constant...
-// -variable can not be used if not declared
-// -identifiers can not start, or include space or a reserved keywords
-// NAMING CONVENTION
-// camelCase or PascalCase 
+//CONCEPT of Overflowing
 
-Float dataType = 1.3f
-Decimal dataType = 23.4m
-Double dataType = 6
+byte number = 225;
 
-the default is double and need to indicate when using decimal or float
+number = number + 1;

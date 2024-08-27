724e0f5cda5ffd5232ecb5bbdec6a835e307d8dc
stevesdiary <stephenoyeyemi@gmail.com>
2024-08-27 10:54:09 +0100

    update metadata

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f9d5db54454b977d3394a2d037ba80bdb51ee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a69a84ce63f849e39e9b00c2befa9826665fbae")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

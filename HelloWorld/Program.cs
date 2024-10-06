4d1e7eb52e8d746e78cd48e2a8cde869c469f7d5
aella-steve <stephenoyeyemi@gmail.com>
2024-10-06 18:57:14 +0100

    Variables and dataTypes

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	class Program 
+	{
+		static void Main(string[] args)
+		{
+			int ourNumber;
+			ourNumber = 2;
+			Console.WriteLine(ourNumber);
+
+			ourNumber = 10;
+			Console.WriteLine(ourNumber);
+
+			ourNumber = 2 + 12;
+			Console.WriteLine(ourNumber);
+
+			double decimalNumber;
+			decimalNumber = 10.15;
+			Console.WriteLine(decimalNumber);
+
+			int number = 5;
+			char myCharacter = 'S';
+			myCharacter += (char) 1;
+			Console.WriteLine(myCharacter);
+
+			string myName= "Ready";
+			Console.WriteLine(myName);
+
+			int newVariable = ourNumber - number;
+			Console.WriteLine(newVariable);
+
+			Boolean temp = true;
+			Console.WriteLine(temp);
+		}
+	}
+}
+// See https://aka.ms/new-console-template for more information
